b56d224593b6604f11765976e82736a5f2f77a22
Timi Adesina <adesinatim@gmail.com>
2025-02-13 23:53:38 +0100

    check tests

--- frontend/src/apis/fetch.users.ts
+++ frontend/src/apis/fetch.users.ts
@@ -25,6 +25,8 @@ export const fetchUsers = async (
 
   const data = await response.json();
   const parsed = ApiResponseSchemaFn(UsersResponseSchema).safeParse(data);
+  console.log({ data });
+  console.log({ parsed });
   if (!parsed.success) {
     throw new Error("Invalid response format");
   }
